cd17ea69d163acc4a9ee21c0cef1b00533053c20
Consultant - Hemant Singh Chonkar <hesich@mcb.mu>
2019-02-14 09:35:52 +0400

    Jenkinsfile

--- src/main/java/com/demo/demoproducer/controller/ProducerController.java
+++ src/main/java/com/demo/demoproducer/controller/ProducerController.java
@@ -3,6 +3,8 @@ package com.demo.demoproducer.controller;
 import java.util.Arrays;
 import java.util.List;
 
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -10,12 +12,36 @@ import org.springframework.web.bind.annotation.RestController;
 import com.demo.demoproducer.bean.Employee;
 
 @RestController
-@RequestMapping("/producer/produceEmployee")
+@RequestMapping("/producer")
+@CrossOrigin("*")
 public class ProducerController {
 	
 
-	@GetMapping
+	@GetMapping("/produceEmployee")
 	public  List<Employee> produceEmployeeList(){
 		return Arrays.asList(new Employee[]{new Employee(1, "Hello There !", "rajesh@gmail.com", 10000000),new Employee(2, "Nitin", "nitin@gmail.com", 123888383)});
 	}
+
+	@GetMapping
+	public ResponseEntity<Message> message() {
+		return ResponseEntity.ok(new Message("Hello this is producer API"));
+	}
+}
+
+
+class Message {
+	String content;
+	Message(String content){
+		this.content = content;
+	}
+
+	public String getContent() {
+		return content;
+	}
+
+	public void setContent(String content) {
+		this.content = content;
+	}
+
+	Message() {}
 }
